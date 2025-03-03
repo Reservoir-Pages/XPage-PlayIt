62d42ec2832fd109901116dbd8d9ff41a4f21b47
Vladimir Nikitin <vreservoirv@rambler.ru>
2025-03-03 16:29:49 +0300

    build done

--- src/scripts/components/menu.js
+++
@@ -1,9 +0,0 @@
-const menuBtn = document.querySelector(".header .header__menu");
-const menu = document.querySelector(".header .nav");
-
-if (menuBtn && menu) {
-	menuBtn.addEventListener("click", (e) => {
-		document.body.classList.toggle("scroll-stop");
-		menu.classList.toggle("is-active");
-	});
-}
